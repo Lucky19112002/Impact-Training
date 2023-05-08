83dd2b51c9752182fffb704bf20c8ba4642d8222
Lucky Pathan <lckpathan@gmail.com>
2023-05-08 12:39:42 +0530

    test

---
+++ Python/Basic_Python_Rules.py
@@ -0,0 +1,25 @@
+a = [1,2,3,4,5,6,7,8,9,10]
+# White spaces not possible
+# print(max a)
+#print(max(a))
+values= 'test','Apple'
+#print(values)
+
+# Data types in python 
+
+List = [1,2,3,4,5]
+Set= {1:'apple',
+      2:'happy'}
+tupless = [1,2,3,4,5,6,7,7,87,]
+Dictinoary = {
+    'Firstname' : '',
+    'Lastname' : ''
+}
+
+Strings = 'apple','nababa','soolep','kinal','kill','alot'
+
+print(List+List)
+print(Set)
+print(tupless)
+print(Dictinoary)
+print(Strings)
